14f89b75f5188d86d0fd48a68066311a5ba5f6ee
DrsBlack <souzacleber.cs.cs@gmail.com>
2025-10-02 22:26:20 -0300

    Header mobile menu functionality changed to JS responsability.

---
+++ assets/js/headerMobileMenu.js
@@ -0,0 +1,8 @@
+let mobileMenuButton = document.querySelector(".submenu .submenubtn");
+let mobileMenu = document.querySelector(".submenu-content");
+
+mobileMenuButton.addEventListener('click', () => {
+mobileMenu.classList.toggle("active");
+} );
+    
+
