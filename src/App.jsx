0fa29ed28540511debf962ef5c442b8e58515cd8
DrsBlack <souzacleber.cs.cs@gmail.com>
2025-10-14 15:24:38 -0300

    Remove title from index.html and add routing for Menu, About, and Contact pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact"
+import Menu from "./pages/Menu"
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/Menu" element={<Menu />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Contact" element={<Contact />} />
             </Routes>
         </BrowserRouter>
     );
