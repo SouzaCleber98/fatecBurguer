588be985ea02a8ee6c39941ba6d94ba90ceb8da7
DrsBlack <souzacleber.cs.cs@gmail.com>
2025-10-15 20:31:38 -0300

    Removing menu.html, starting the migration of the menu to React

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,137 @@
+import Header from "../components/layout/Header";
+
+const sampleItems = [
+    {
+        id: "1",
+        title: "Burger 1",
+        description: "Duis semper in metus eu suscipit. Aliquam erat volutpat.",
+        price: "R$ 25,00",
+        image: "https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+    },
+    {
+        id: "2",
+        title: "Burger 2",
+        description: "Tasty burger with cheese and bacon.",
+        price: "R$ 28,00",
+        image: "https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+    },
+    {
+        id: "3",
+        title: "Burger 3",
+        description: "Tasty burger with cheese and bacon.",
+        price: "R$ 27,00",
+        image: "https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+    },
+    {
+        id: "4",
+        title: "Burger 4",
+        description: "Tasty burger",
+        price: "R$ 30,00",
+        image: "https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+    },
+];
+
+function Menu() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
+export default Menu;
+
+
+
+//     <main>
+//         <div class="cardapio-container">
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//             <div class="item">
+//                 <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?auto=format&fit=crop&w=800&q=80"
+//                     alt="Lanche 1">
+//                 <div class="buy-button">Comprar</div>
+//                 <div class="price">R$ 000,00</div>
+//                 <div class="info">
+//                     <h3>Lanche 1</h3>
+//                     <p>Duis semper in metus eu suscipit. Aliquam erat volutpat.</p>
+//                 </div>
+//             </div>
+//         </div>
+//     </main>
+
+//     <script type="module" src="./assets/js/menuBuyClick.js"></script>
+//     <script src="./assets/js/headerMobileMenu.js"></script>
+//     <script src="./assets/js/darkModeToggle.js"></script>
+
+// </body>
+
+// </html>
