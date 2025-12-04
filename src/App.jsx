2ed7d8b1c7fbcbff460155db04f33db1d3a85971
CleberSouza <souzacleber.cs.cs@gmail.com>
2025-12-04 00:06:11 -0300

    Add <Header /> to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact"
-import Menu from "./pages/Menu"
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Menu" element={<Menu />} />
-                <Route path="/About" element={<About />} />
-                <Route path="/Contact" element={<Contact />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
